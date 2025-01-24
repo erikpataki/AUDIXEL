fc88dd25ef900a153f1d5ec146ccc252bcd7f2c0
erikpataki <erikpataki2003@gmail.com>
2025-01-24 01:06:54 +0000

    forgot to git add

---
+++ src/components/Dropdowns/Slider/Slider.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Slider.css';
+
+const Slider = ({ label, value, setValue }) => {
+    return (
+        <div className="slider-parent">
+            <label className="slider-label">{label}</label>
+            <div className="threshold-control">
+                <input
+                    type="range"
+                    min="0"
+                    max="255"
+                    value={value}
+                    onChange={(e) => setValue(Number(e.target.value))}
+                    className='slider-range'
+                />
+                <input
+                    type="number"
+                    min="0"
+                    max="255"
+                    value={value}
+                    onChange={(e) => {
+                        const value = Math.min(255, Math.max(0, Number(e.target.value)));
+                        setValue(value);
+                    }}
+                    className="slider-number"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
