46cc0891c67f82e60a3b3edf4d6fb619fff8bb33
erikpataki <erikpataki2003@gmail.com>
2025-02-21 21:39:22 +0000

    reverted to previous save before any essentia integration was done

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "path": require.resolve("path-browserify"),
-      "fs": false,
-      "buffer": require.resolve("buffer/"),
-    };
-    return config;
-  }
