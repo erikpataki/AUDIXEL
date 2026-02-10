563e70d13394df0ec4a5badd7e7017e306f82ddd
Erik Pataki <124370437+erikpataki@users.noreply.github.com>
2026-02-10 08:30:08 +0000

    Further improvements on tutorial message

--- src/utils/tutorialContent.js
+++ src/utils/tutorialContent.js
@@ -7,10 +7,10 @@ export const getTutorialMessage = () => (
     <h2>What does AUDIXEL do?</h2>
     <p>AUDIXEL takes your songs and processes them to create abstract images. Here's a simple breakdown of how the process works:</p>
     <ol>
-      <li><strong>Upload your music:</strong> Start by selecting a music file (.wav, .mp3, or .ogg) from your computer.</li>
-      <li><strong>Music analysis:</strong> AUDIXEL will analyse various features of the music, such as its energy, pitch, and rhythm.</li>
-      <li><strong>Image generation:</strong> Based on the music analysis, AUDIXEL generates an image. The visual elements of the image are influenced by the music's features. For example, a high-energy song might produce vibrant, dynamic visuals, while a calm, soothing track might result in a more subdued, relaxed pattern.</li>
-      <li><strong>Customisation:</strong> You can tweak various settings to customise the generated image. Adjust the amount of pixel sorting, change the angle of the visual elements, and select different sorting modes (like brightness, hue, or saturation) to get the perfect result you're looking for.</li>
+      <li><strong>Upload your music:</strong> Start by selecting a music file (.wav, .mp3, or .ogg) from your device.</li>
+      <li><strong>Music analysis:</strong> AUDIXEL will analyse different characteristics of the audio file.</li>
+      <li><strong>Image generation:</strong> Based on the analysis, AUDIXEL generates an image. The visual elements of the image are influenced by the audio's features. For example, a high-energy song might produce vibrant, dynamic visuals, while a calm, soothing track might result in a more subdued, relaxed pattern.</li>
+      <li><strong>Customisation:</strong> You can tweak various settings to customise the generated image. Adjust the intensity or angle of the pixel sorting, select different sorting modes (like brightness, hue, or saturation), or even change the outputted resolution to get the perfect result you're looking for.</li>
       <li><strong>Download and share:</strong> Once you're happy with the generated image, you can download it and use it in whatever way you wish.</li>
     </ol>
     
